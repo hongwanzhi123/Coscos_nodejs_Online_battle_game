01710e08ed5af123e65ff1f15b4fa2deec127e7c
hongwanzhi123 <2023276501@qq.com>
2026-03-29 20:02:28 +0800

    add server Enum files

---
+++ apps/server/src/Common/Enum.ts
@@ -0,0 +1,15 @@
+
+export enum InputTypeEnum{
+    ActorMove = "ActorMove",
+    WeaponShoot = "WeaponShoot",
+    TimePast = "TimePast",
+}
+
+export enum EntityTypeEnum{
+    Actor1 = "Actor1",
+    Map = "Map",
+    Weapon1 = "Weapon1",
+    Bullet1 = "Bullet1",
+    Bullet2 = "Bullet2",
+    Explosion = "Explosion",
+}
